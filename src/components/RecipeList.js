4d6fdaf967be43236120c035865be6af5f6a5d51
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-15 09:54:43 -0400

    Input content of recipe in card

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 // styles
 import './RecipeList.css'
 
@@ -5,7 +7,11 @@ export default function RecipeList({ recipes }) {
   return (
     <div className='recipe-list'>
       {recipes.map(recipe => (
-        <div key={recipe.id}>
+        <div key={recipe.id} className='card'>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.cookingTime} to cook.</p>
+          <div>{recipe.method.substring(0, 100)}...</div>
+          <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
         </div>
       ))}
     </div>
