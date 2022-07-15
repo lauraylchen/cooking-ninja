7f26cde196786a299581194573379396e97197d4
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-15 14:07:53 -0400

    Able to fetch a specific recipe with id

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,10 +1,25 @@
+import { useParams } from "react-router-dom";
+
 // style
 import './Recipe.css'
 
+// hooks
+import { useFetch } from '../../hooks/useFetch'
+
 export default function Recipe() {
+  const { id } = useParams()
+  const url = 'http://localhost:3000/recipes/' + id
+  const { data: recipe, isPending, error } = useFetch(url)
+
   return (
-    <div>
-      Recipe
+    <div className="recipe">
+      {error && <p className="error">{error}</p> }
+      {isPending && <p className="loading">Loading ...</p>}
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+        </>
+      )}
     </div>
   )
 }
