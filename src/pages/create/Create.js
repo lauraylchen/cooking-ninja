6a182d459a5b093801a921cb62e4aafe44ef11dd
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-17 13:10:50 -0400

    Added input field to add ingredients

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 // styles
 import './Create.css'
@@ -7,10 +7,23 @@ export default function Create() {
   const [title, setTitle] = useState('')
   const [method, setMethod] = useState('')
   const [cookingTime, setCookingTime] = useState('')
+  const [newIngredient, setNewIngredient] = useState('')
+  const [ingredients, setIngredients] = useState([])
+  const ingredientInput = useRef(null)
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(title, method, cookingTime)
+    console.log(title, method, cookingTime, ingredients)
+  }
+
+  const handleAdd = (e) => {
+    e.preventDefault()
+    const ing = newIngredient.trim()
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients(prevIngredients => [...prevIngredients, ing])
+    }
+    setNewIngredient('')
+    ingredientInput.current.focus()
   }
 
   return (
@@ -26,6 +39,18 @@ export default function Create() {
             value={title}
             required
           />
+        </label>
+        <label>
+          <span>Recipe Ingredients:</span>
+          <div className='ingredients'>
+            <input
+              type="text"
+              onChange={(e) => setNewIngredient(e.target.value)}
+              value={newIngredient}
+              ref={ingredientInput}
+            />
+            <button onClick={handleAdd} className='btn'>Add</button>
+          </div>
         </label>
         <label>
           <span>Recipe Method:</span>
