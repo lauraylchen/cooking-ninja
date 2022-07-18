fb4e53e5ec5516a90b2ec16549577e8f4fa44e43
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-18 00:11:57 -0400

    Fixed history to be function

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -12,7 +12,7 @@ export default function Create() {
   const [newIngredient, setNewIngredient] = useState('')
   const [ingredients, setIngredients] = useState([])
   const ingredientInput = useRef(null)
-  const history = useHistory
+  const history = useHistory()
 
   const { postData, data } = useFetch('http://localhost:3000/recipes', 'POST')
 
