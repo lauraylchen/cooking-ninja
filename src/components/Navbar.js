61f2006ca0973acabf49bb7d50220cb805a8a907
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-18 10:19:20 -0400

    Added Searchbar into Navbar and added style CSS

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom'
 // styles
 import './Navbar.css'
 
+// components
+import Searchbar from './Searchbar'
+
 export default function Navbar() {
   return (
     <div className='navbar'>
@@ -10,7 +13,10 @@ export default function Navbar() {
         <Link to='/' className='brand'>
           <h1>Cooking Ninja</h1>
         </Link>
-        <Link to='/create'>Create Recipe</Link>
+        <div className='choices'>
+          <Searchbar />
+          <Link to='/create'>Create Recipe</Link>
+        </div>
       </nav>
     </div>
   )
