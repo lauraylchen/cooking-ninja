5ff7844f61a222475655e6fbf5bbf5366d5c132a
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-18 23:38:36 -0400

    Created a custom Provider function that takes a children prop

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,3 +1,12 @@
 import { createContext } from "react"
 
 export const ThemeContext = createContext()
+
+export function ThemeProvider({children}) {
+
+  return (
+    <ThemeContext.Provider>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
