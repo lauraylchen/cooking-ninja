bdc713fa3ec27b6f7db21c1929981481d739169d
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-22 15:28:15 -0400

    set navbar background color to blue using custom context hook

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -5,7 +5,7 @@ export const ThemeContext = createContext()
 export function ThemeProvider({children}) {
 
   return (
-    <ThemeContext.Provider>
+    <ThemeContext.Provider value={{ color: 'blue' }}>
       {children}
     </ThemeContext.Provider>
   )
