1fb59df7823180fe2dfb4f0faf68f5fde59ed41b
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-22 16:50:20 -0400

    Added content for component by mapping to three colors

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -3,12 +3,22 @@ import { useTheme } from '../hooks/useTheme'
 // styles
 import './ThemeSelector.css'
 
+const themeColors = ['#58249C', '#249C6B', '#B70233']
+
 export default function ThemeSelector() {
   const { changeColor } = useTheme()
 
   return (
-    <div>
-
+    <div className='theme-selector'>
+      <div className='theme-buttons'>
+        {themeColors.map(color => (
+          <div
+            key={color}
+            onClick={() => changeColor(color)}
+            style={{ background: color }}
+          />
+        ))}
+      </div>
     </div>
   )
 }
