a5c173eb24ac991d3d3e5f49ef0671d208776b84
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-25 14:31:10 -0400

    Added svg icon into theme selector component

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -1,4 +1,5 @@
 import { useTheme } from '../hooks/useTheme'
+import modeIcon from '../assets/mode-icon.svg'
 
 // styles
 import './ThemeSelector.css'
@@ -6,10 +7,21 @@ import './ThemeSelector.css'
 const themeColors = ['#58249C', '#249C6B', '#B70233']
 
 export default function ThemeSelector() {
-  const { changeColor } = useTheme()
+  const { changeColor, changeMode, mode } = useTheme()
+
+  const toggleMode = () => {
+    changeMode(mode === 'dark' ? 'light' : 'dark')
+  }
 
   return (
     <div className='theme-selector'>
+      <div className="mode-toggle">
+        <img
+          src={modeIcon}
+          alt="Dark/Light Mode Icon"
+          onClick={toggleMode}
+        />
+      </div>
       <div className='theme-buttons'>
         {themeColors.map(color => (
           <div
