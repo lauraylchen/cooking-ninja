024e0efbf616ce20e9d4dd9411c82af4d245a918
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-25 15:29:06 -0400

    Added styling for dark mode background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { useTheme } from './hooks/useTheme';
 
 // pages components
 import Home from './pages/home/Home'
@@ -14,8 +15,10 @@ import ThemeSelector from './components/ThemeSelector';
 import './App.css'
 
 function App() {
+  const { mode } = useTheme()
+
   return (
-    <div className="App">
+    <div className={`App ${mode}`}>
       <BrowserRouter>
         <Navbar />
         <ThemeSelector />
