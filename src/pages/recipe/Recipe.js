cfd4f5b9b1bfb38bfd96ee4707a325f516a9d1a0
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-25 15:46:10 -0400

    Added dark mode css to Recipe page

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -5,14 +5,16 @@ import './Recipe.css'
 
 // hooks
 import { useFetch } from '../../hooks/useFetch'
+import { useTheme } from '../../hooks/useTheme'
 
 export default function Recipe() {
   const { id } = useParams()
   const url = 'http://localhost:3000/recipes/' + id
   const { data: recipe, isPending, error } = useFetch(url)
+  const { mode } = useTheme()
 
   return (
-    <div className="recipe">
+    <div className={`recipe ${mode}`}>
       {error && <p className="error">{error}</p> }
       {isPending && <p className="loading">Loading ...</p>}
       {recipe && (
